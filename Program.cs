764bcddc988676b191fb9b54c988b8a3e2059354
Yan-91 <villagepllayer@gmail.comgit config --global user.name  Yan-91git config --global user.email villagepllayer@gmail.com>
2020-06-16 15:18:15 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula18Dojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Jogador Cr7 = new Jogador();
+            Cr7.Nome= "messi";
+            Cr7.Altura = 1.80f;
+           Cr7.Peso = 80f;
+            Cr7.Nascimento = DateTime.Parse("24/06/1987");
+           Cr7.Posicao = "Atacante";
+            
+            Console.WriteLine(Cr7.CalcularIdade());
+            Console.WriteLine(Cr7.VerificarAposentadoria());
+            
+        }
+    
+        
+    }
+}
